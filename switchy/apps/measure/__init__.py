5e80168e606027f7bc9efe6f51a7dfd0d76e38f7
Tyler Goodlet <tgoodlet@gmail.com>
2015-03-30 18:27:48 -0400

    Move all measurement related modules into a sub package

---
+++ switchy/apps/measure/__init__.py
@@ -0,0 +1,61 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Measurements app for collecting call latency and performance stats.
+"""
+import weakref
+from ..marks import event_callback
+from .. import utils
+from metrics import new_array
+
+
+class Metrics(object):
+    """Collect call oriented measurements
+
+    Only an instance of this class can be loaded as switchy app
+    """
+    def __init__(self, listener=None, array=None, pool=None):
+        self.listener = weakref.proxy(listener) if listener else None
+        self._array = array if array else new_array()  # np default buffer
+        self.log = utils.get_logger(__name__)
+        self.pool = pool if pool else self.listener
+
+    def prepost(self, listener, array=None, pool=None):
+        if array is not None:  # array can be overriden at app load time
+            self._array = array
+        if not self.listener:
+            self.listener = listener
+        self.pool = pool if pool else self.listener
+        yield
+        self.listener = None
+        self.slaves = None
+
+    @property
+    def array(self):
+        return self._array
+
+    @event_callback('CHANNEL_HANGUP')
+    def log_stats(self, sess, job):
+        """Append measurement data inserting only once per call
+        """
+        # TODO: eventually we should use a data type which allows the
+        # separation of metrics between those that require two vs. one
+        # leg to calculate?
+        l = self.listener
+        pool = self.pool
+        if sess.call.sessions:
+            # the `callee` UA is who's time measures we want
+            partner_sess = sess.call.sessions[-1]
+            if l.sessions.get(partner_sess.uuid, False):
+                rollover = self._array.insert((
+                    sess.create_time - l._epoch if l else sess.create_time,
+                    abs(sess.create_time - partner_sess.create_time),
+                    abs(sess.answer_time - partner_sess.answer_time),
+                    abs(sess.answer_time - sess.create_time),
+                    sess.originate_time - job.launch_time if job else 0,
+                    pool.count_failed() if pool else 0,
+                    pool.count_sessions(),
+                ))
+                if rollover:
+                    self.log.warn('resetting metric buffer index!')
--- switchy/apps/measure/metrics.py
+++ switchy/apps/measure/metrics.py
@@ -2,18 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-Measurements app for collecting call latency and performance stats.
-
-This module includes helper classes for taking load measurements using numpy.
+This module includes helpers for capturing measurements using numpy.
 """
-import logging
-import weakref
-from ..marks import event_callback
-from .. import utils
 import numpy as np
-import glob
-import os
-from collections import OrderedDict, defaultdict
+from switchy import utils
 
 
 # numpy ndarray template
@@ -128,54 +120,3 @@ def new_array(dtype=metric_dtype, size=2**20):
     """Return a new capped numpy array
     """
     return CallMetrics(np.zeros(size, dtype=dtype))
-
-
-class Metrics(object):
-    """Collect call oriented measurements
-
-    Only an instance of this class can be loaded as switchy app
-    """
-    def __init__(self, listener=None, array=None, pool=None):
-        self.listener = weakref.proxy(listener) if listener else None
-        self._array = array if array else new_array()  # np default buffer
-        self.log = utils.get_logger(__name__)
-        self.pool = pool if pool else self.listener
-
-    def prepost(self, listener, array=None, pool=None):
-        if array is not None: # array can be overriden at app load time
-            self._array = array
-        if not self.listener:
-            self.listener = listener
-        self.pool = pool if pool else self.listener
-        yield
-        self.listener = None
-        self.slaves = None
-
-    @property
-    def array(self):
-        return self._array
-
-    @event_callback('CHANNEL_HANGUP')
-    def log_stats(self, sess, job):
-        """Append measurement data inserting only once per call
-        """
-        # TODO: eventually we should use a data type which allows the
-        # separation of metrics between those that require two vs. one
-        # leg to calculate?
-        l = self.listener
-        pool = self.pool
-        if sess.call.sessions:
-            # the `callee` UA is who's time measures we want
-            partner_sess = sess.call.sessions[-1]
-            if l.sessions.get(partner_sess.uuid, False):
-                rollover = self._array.insert((
-                    sess.create_time - l._epoch if l else sess.create_time,
-                    abs(sess.create_time - partner_sess.create_time),
-                    abs(sess.answer_time - partner_sess.answer_time),
-                    abs(sess.answer_time - sess.create_time),
-                    sess.originate_time - job.launch_time if job else 0,
-                    pool.count_failed() if pool else 0,
-                    pool.count_sessions(),
-                ))
-                if rollover:
-                    self.log.warn('resetting metric buffer index!')
