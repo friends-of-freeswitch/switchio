2d2978a0ec8a73205936ce46fa0955f63e808783
Tyler Goodlet <tgoodlet@gmail.com>
2015-11-13 12:10:37 -0500

    Add copyright info

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python2.7
+#
+# Copyright 2014 Sangoma Technologies Inc.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from setuptools import setup
 
 
