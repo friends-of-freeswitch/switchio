cd4d0c633be532aa0ce8a7793514ea631f087dd2
Tyler Goodlet <tgoodlet@gmail.com>
2016-08-08 09:55:11 -0400

    Add a `Service` type
    Allows for launching long running apps which control a
    FreeSWITCH cluster. We can probably factor a bunch of duplicate
    functionality out of the `Originator` into this :)

--- switchy/__init__.py
+++ switchy/__init__.py
@@ -2,13 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-Switchy
-Licensed under the MPL license (see `LICENSE` file)
+switchy: A FreeSWITCH control and stress testing micro framework.
 
-Fast FreeSWITCH ESL control with a focus on load testing.
-
-The `EventListener` was inspired from Moises Silva's
-'fs_test' project: https://github.com/moises-silva/fs_test
+Licensed under the MPL 2.0 license (see `LICENSE` file)
 """
 import apps
 from os import path
@@ -19,6 +15,8 @@ from distribute import SlavePool, MultiEval
 from marks import event_callback, handler
 from connection import Connection, ConnectionError
 from sync import sync_caller
+from serve import Service
+
 
 __package__ = 'switchy'
 __version__ = '0.1.alpha'
---
+++ switchy/serve.py
@@ -0,0 +1,47 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Server components for building clustered call processing systems.
+"""
+from . import utils
+from .apps import AppManager
+from .observe import get_pool
+
+
+class Service(object):
+    """Serve centralized, long running, call processing apps on top of a
+    FreeSWITCH cluster.
+    """
+    def __init__(self, contacts):
+        self.pool = get_pool(
+            contacts, call_tracking_header='variable_call_uuid')
+        self.apps = AppManager(self.pool)
+        self.host = self.pool.evals('listener.host')
+        self.log = utils.get_logger(utils.pstr(self))
+        # initialize all reactor event loops
+        self.pool.evals('listener.connect()')
+
+    def run(self, block=True):
+        """Run service optionally blocking until stopped.
+        """
+        self.pool.evals('listener.start()')
+        assert all(self.pool.evals('listener.is_alive()'))
+        if block:
+            try:
+                self.pool.evals('listener.wait()')
+            except KeyboardInterrupt:
+                pass
+            finally:
+                self.stop()
+
+    def is_alive(self):
+        """Return bool indicating if a least one event loop is alive.
+        """
+        return any(self.pool.evals('listener.is_alive()'))
+
+    def stop(self):
+        """Stop service and disconnect.
+        """
+        self.pool.evals('listener.disconnect()')
+        self.pool.evals('listener.wait(1)')
