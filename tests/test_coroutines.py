e299a2311c5452864633d169825f5616cb57dcce
Tyler Goodlet <tgoodlet@gmail.com>
2017-11-07 17:47:09 -0500

    Add coroutine timeout test

--- tests/test_coroutines.py
+++ tests/test_coroutines.py
@@ -4,12 +4,14 @@ Helpers for testing py3.5+ ``asyncio`` functionality.
 This module is mostly to avoid syntax errors in modules still used for
 py2.7 testing.
 """
+import pytest
+import asyncio
 import time
 from switchio import sync_caller
 from switchio import coroutine
 
 
-def test_cancel_coro(fsip):
+def test_coro_cancel(fsip):
     """Verify that if a call never receives an event which is being
     waited that the waiting coroutine is cancelled at call hangup.
     """
@@ -50,3 +52,39 @@ def test_cancel_coro(fsip):
         assert br_fut.cancelled()
         assert not callee._futures  # should be popped by done callback
         assert el.count_calls() == 0
+
+
+def test_coro_timeout(fsip):
+    """Verify that if a call never receives an event which is being
+    waited that the waiting coroutine is cancelled at call hangup.
+    """
+    class MyApp:
+        @coroutine("CHANNEL_CREATE")
+        async def timeout_on_hangup(self, sess):
+            # should never arrive since we don't subscribe for the event type
+            if sess.is_inbound():
+                await sess.answer()
+                sess.vars['answered'] = True
+                await sess.recv("CHANNEL_HANGUP", timeout=1)
+
+    with sync_caller(fsip, apps={"MyApp": MyApp}) as caller:
+        # have the external prof call itself by default
+        assert 'MyApp' in caller.app_names
+        sess, waitfor = caller(
+            "doggy@{}:{}".format(caller.client.host, 5080),
+            'MyApp',
+            timeout=3,
+        )
+        assert sess.is_outbound()
+        callee = sess.call.get_peer(sess)
+        callee_futs = callee._futures
+        waitfor(callee, 'answered', timeout=0.2)
+        # answer future should be popped
+        assert not callee_futs.get('CHANNEL_ANSWER')
+        hangup_fut = callee_futs.get('CHANNEL_HANGUP')
+        assert hangup_fut
+        time.sleep(1)  # wait for timeout
+        task = callee.tasks.pop(hangup_fut)[0]
+        assert task.done()
+        with pytest.raises(asyncio.TimeoutError):
+            task.result()
