095ce9ee99e6676353a9144ac8497c7b5e2e2947
Matthew Williams <mgwilliams@gmail.com>
2019-12-26 13:41:36 -0500

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name="switchio",
-    version='0.1.0.a4~mgwilliams',
+    version='0.1.0.a5~mgwilliams',
     description='asyncio powered FreeSWITCH cluster control',
     long_description=readme,
     license='Mozilla',
